1629b2721a1ec5e95cec98c5870bac167acb66b0
Ricardo Viana <rsoaresv@kenvue.com>
2025-05-11 19:34:17 -0300

    Update

--- wp-content/themes/up/partials/home/gallery.php
+++ wp-content/themes/up/partials/home/gallery.php
@@ -1,34 +1,34 @@
 <?php
-$up_home_gallery  = get_field('up_home_gallery', 'option');
+$up_home_gallery = get_field('up_home_gallery', 'option');
 $gallery_items = !empty($up_home_gallery['up_home_gallery_images']) ? $up_home_gallery['up_home_gallery_images'] : [];
-if($gallery_items):
+if ($gallery_items):
   ?>
 
   <div class="gallery">
-      <div class="lines"></div>
-      <h2 class="uppercase home-title"><?php echo __('Galeria', 'up'); ?></h2>
+    <div class="lines"></div>
+    <h2 class="uppercase home-title"><?php echo __('Galeria', 'up'); ?></h2>
     <div class="gallery-items">
       <?php
       $count = 0;
       foreach ($gallery_items as $key => $img):
         $count++;
-        if($count === 6): ?>
-            <div class="cta">
-                <a href="https://www.flickr.com/photos/universoproducao/" class="flex" target="_blank">
+        if ($count === 6): ?>
+          <div class="cta">
+            <a href="https://www.flickr.com/photos/universoproducao/" class="flex" target="_blank">
                     <span>
                       Ir para o Flickr da
-                      <br />
+                      <br/>
                       Universo Produção
                     </span>
-                    <span class="icon-next"></span>
-                </a>
-            </div>
+              <span class="icon-next"></span>
+            </a>
+          </div>
         <?php endif; ?>
         <div class="">
           <a data-fancybox="gallery" href="<?php echo wp_get_attachment_image_url($img['ID'], 'full') ?>">
             <?php echo wp_get_attachment_image($img['ID'], 'gallery') ?></a>
         </div>
-        <?php endforeach; ?>
+      <?php endforeach; ?>
     </div>
   </div>
 <?php endif;
