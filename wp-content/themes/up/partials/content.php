9b8e975a44c755b53a117d5620e8596604d7e963
Ricardo Viana <rsoaresv@kenvue.com>
2025-06-23 18:41:00 -0300

    Update

--- wp-content/themes/up/partials/content.php
+++ wp-content/themes/up/partials/content.php
@@ -1,19 +1,17 @@
 <div class="main-container container flex-col">
   <?php get_template_part('partials/heading', '', $args); ?>
-    <div class="main-content">
-      <div class="container container-small content flex-col">
-        <?php
-        if(!empty($args['content-before'])) {
-          echo $args['content-before'];
-        }
+  <div class="container container-small content flex-col">
+    <?php
+    if(!empty($args['content-before'])) {
+      echo $args['content-before'];
+    }
 
-        echo the_content();
+    echo the_content();
 
-        if(!empty($args['content-after'])) {
-          echo $args['content-after'];
-        }
-        ?>
-      </div>
-    </div>
+    if(!empty($args['content-after'])) {
+      echo $args['content-after'];
+    }
+    ?>
+  </div>
 </div>
 <?php
