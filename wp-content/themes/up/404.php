8f2059b805a926bd0aad4a35018fd39247c651ce
Ricardo Viana <rsoaresv@kenvue.com>
2025-06-23 18:37:44 -0300

    Update

---
+++ wp-content/themes/up/404.php
@@ -0,0 +1,7 @@
+<?php get_header(); ?>
+  <section class="error-404 not-found">
+    <h1>Página não encontrada</h1>
+    <p>Desculpe, a página que você procura não existe ou foi removida.</p>
+    <a class="btn-red mt-8" href="<?php echo home_url(); ?>">Voltar para a página inicial</a>
+  </section>
+<?php get_footer();
