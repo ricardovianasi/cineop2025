02a8aab4e19730908c38a001f4f5db2652268e38
Ricardo Viana <rsoaresv@kenvue.com>
2025-06-24 14:04:16 -0300

    Update

--- wp-content/themes/up/header.php
+++ wp-content/themes/up/header.php
@@ -54,15 +54,15 @@ $up_general_header_cta_url = $up_general_header_cta['url'] ?? false;
   </script>
 
   <?php wp_head(); ?>
-
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-10028533-1"></script>
+  
+  <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-2D766434XR"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-10028533-1');
+    gtag('config', 'G-2D766434XR');
   </script>
 
   <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/assets/dist/styles/vendor.css">
